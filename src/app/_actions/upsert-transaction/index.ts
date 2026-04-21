79caf0629d465a1f3bac12b36f6a6bad44677528
Ana-Karoline-Tech <anna.karolinawork@gmail.com>
2026-04-21 15:05:46 -0300

    feat: adicionar autenticação ao upsert de transações com verificação de sessão do usuário

--- src/app/_actions/upsert-transaction/index.ts
+++ src/app/_actions/upsert-transaction/index.ts
@@ -3,12 +3,27 @@
 import { db } from "@/src/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { upsertTransactionSchema, UpsertTransactionData } from "../../_schemas/transactions";
+import { auth } from "@/src/lib/auth";
+import { headers } from "next/headers";
 
 export const upsertTransaction = async (params: UpsertTransactionData) => {
-  upsertTransactionSchema.parse(params);
-  await db.transaction.create({
-    data: params,
+  const session = await auth.api.getSession({
+    headers: await headers()
   });
+
+  if (!session) {
+    throw new Error("Usuário não autenticado");
+  }
+
+  upsertTransactionSchema.parse(params);
+  
+  await db.transaction.create({
+    data: {
+        ...params,
+        userId: session.user.id
+    },
+  });
+
   revalidatePath("/");
   revalidatePath("/transactions");
 };
